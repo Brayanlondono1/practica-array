bf71c0618efdbcf65c66ad8e58ff63c314dbeb2d
brayan <londonobrayan372@gmail.com>
2024-03-05 22:27:35 -0500

    array con diferentes elementos

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let arreglovacio = []; // Array vacío
 
 let arregloconelementos = [poker,club,costeña]; // array con elementos
+
+
+let arreglocondiferentes = [55, "brayan", {nombre: "valentina"}, ["negro", "rosado", "azul"]];
